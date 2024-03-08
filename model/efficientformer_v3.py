2673816d875c6d3e272bdfeb4e80aeea1ca86a54
Zsombor Szenyán <zsomborszenyan@gmail.com>
2024-03-08 14:38:09 +0100

    coreml conversion and FNN in efficientformer

--- model/efficientformer_v3.py
+++ model/efficientformer_v3.py
@@ -1,4 +1,100 @@
-from torch.nn import Module
+from torch import nn
+import torch
 
-class EfficientFormerV3(Module):
+class EfficientFormerV3(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.patch_embed = nn.Sequential(
+            nn.Conv2d(3, 32 // 2, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(32 // 2),
+            nn.GELU(),
+            nn.Conv2d(32 // 2, 32, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(32),
+            nn.GELU(),
+        )
+
+        self.blocks = []
+
+        self.blocks.append(FFN(
+                32, mlp_ratio=4,
+                act_layer=nn.GELU,
+                drop=0.,
+                use_layer_scale=True,
+                layer_scale_init_value=1e-5,
+            ))
+
+    def forward(self, x):
+        x = self.patch_embed(x)
+        return x
+
+class FFN(nn.Module):
+    def __init__(self, dim, mlp_ratio=4.,
+                 act_layer=nn.GELU,
+                 drop=0.,
+                 use_layer_scale=True, layer_scale_init_value=1e-5):
+        super().__init__()
+
+        mlp_hidden_dim = int(dim * mlp_ratio)
+        self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim,
+                       act_layer=act_layer, drop=drop, mid_conv=True)
+
+        self.use_layer_scale = use_layer_scale
+        if use_layer_scale:
+            self.layer_scale_2 = nn.Parameter(
+                layer_scale_init_value * torch.ones(dim).unsqueeze(-1).unsqueeze(-1), requires_grad=True)
+
+    def forward(self, x):
+        if self.use_layer_scale:
+            x = x + self.layer_scale_2 * self.mlp(x)
+        else:
+            x = x + self.mlp(x)
+        return x
     
+class Mlp(nn.Module):
+    """
+    Implementation of MLP with 1*1 convolutions.
+    Input: tensor with shape [B, C, H, W]
+    """
+
+    def __init__(self, in_features, hidden_features=None,
+                 out_features=None, act_layer=nn.GELU, drop=0., mid_conv=False):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        self.mid_conv = mid_conv
+        self.fc1 = nn.Conv2d(in_features, hidden_features, 1)
+        self.act = act_layer()
+        self.fc2 = nn.Conv2d(hidden_features, out_features, 1)
+        self.drop = nn.Dropout(drop)
+        self.apply(self._init_weights)
+
+        if self.mid_conv:
+            self.mid = nn.Conv2d(hidden_features, hidden_features, kernel_size=3, stride=1, padding=1,
+                                 groups=hidden_features)
+            self.mid_norm = nn.BatchNorm2d(hidden_features)
+
+        self.norm1 = nn.BatchNorm2d(hidden_features)
+        self.norm2 = nn.BatchNorm2d(out_features)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Conv2d):
+            trunc_normal_(m.weight, std=.02)
+            if m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.norm1(x)
+        x = self.act(x)
+
+        if self.mid_conv:
+            x_mid = self.mid(x)
+            x_mid = self.mid_norm(x_mid)
+            x = self.act(x_mid)
+        x = self.drop(x)
+
+        x = self.fc2(x)
+        x = self.norm2(x)
+
+        x = self.drop(x)
+        return x
