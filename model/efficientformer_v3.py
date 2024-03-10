b88d3e60b31a5a8b5ce41ddd99c3f88052c507b7
Zsombor Szenyán <zsomborszenyan@gmail.com>
2024-03-10 10:48:40 +0100

    Update EfficientFormerV3 model and add training and testing loops

--- model/efficientformer_v3.py
+++ model/efficientformer_v3.py
@@ -1,100 +1,106 @@
 from torch import nn
 import torch
+from efficientformer_v2 import EfficientFormerV2, EfficientFormer_depth, EfficientFormer_width, expansion_ratios_S0
 
 class EfficientFormerV3(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self, batch_size = 32) -> None:
         super().__init__()
-        self.patch_embed = nn.Sequential(
-            nn.Conv2d(3, 32 // 2, kernel_size=3, stride=2, padding=1),
-            nn.BatchNorm2d(32 // 2),
-            nn.GELU(),
-            nn.Conv2d(32 // 2, 32, kernel_size=3, stride=2, padding=1),
-            nn.BatchNorm2d(32),
-            nn.GELU(),
+
+        self.batch_size = batch_size
+
+        efficientformer = EfficientFormerV2(
+            layers = EfficientFormer_depth['S0'],
+            embed_dims=EfficientFormer_width['S0'],
+            downsamples=[True, True, True, True, True],
+            vit_num=2,
+            drop_path_rate=0.0,
+            e_ratios=expansion_ratios_S0,
+            num_classes=0,
+            resolution=218,
+            distillation=False
+        )
+        self.network = nn.Sequential(
+            efficientformer,
+            Head()
         )
 
-        self.blocks = []
-
-        self.blocks.append(FFN(
-                32, mlp_ratio=4,
-                act_layer=nn.GELU,
-                drop=0.,
-                use_layer_scale=True,
-                layer_scale_init_value=1e-5,
-            ))
 
     def forward(self, x):
-        x = self.patch_embed(x)
-        return x
+        return self.network(x)
 
-class FFN(nn.Module):
-    def __init__(self, dim, mlp_ratio=4.,
-                 act_layer=nn.GELU,
-                 drop=0.,
-                 use_layer_scale=True, layer_scale_init_value=1e-5):
+def head_block(output_features):
+    num_features = EfficientFormer_width['S0'][-1]
+    return nn.Sequential(
+        nn.Linear(num_features, 2048),
+        nn.ReLU(),
+        nn.Linear(2048, 512),
+        nn.ReLU(),
+        nn.Linear(512, output_features),
+        nn.Sigmoid()
+    )
+
+
+class Head(nn.Module):
+    def __init__(self) -> None:
         super().__init__()
-
-        mlp_hidden_dim = int(dim * mlp_ratio)
-        self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim,
-                       act_layer=act_layer, drop=drop, mid_conv=True)
-
-        self.use_layer_scale = use_layer_scale
-        if use_layer_scale:
-            self.layer_scale_2 = nn.Parameter(
-                layer_scale_init_value * torch.ones(dim).unsqueeze(-1).unsqueeze(-1), requires_grad=True)
+        groups = [9, 10, 5, 2, 9, 3, 2]
+        self.blocks = nn.ModuleList(
+            [head_block(group) for group in groups]
+        )
 
     def forward(self, x):
-        if self.use_layer_scale:
-            x = x + self.layer_scale_2 * self.mlp(x)
-        else:
-            x = x + self.mlp(x)
-        return x
+        output = [block(x) for block in self.blocks]
+        output = torch.cat(output, dim=1)
+        return output
+
+def train_loop(dataloader, model, loss_fn, optimizer, device):
+    size = len(dataloader.dataset)
+    model.train()
+    for batch, (x, y) in enumerate(dataloader):
+        x = x.to(device)
+        y = transform_y(y)
+        y = y.to(device)
     
-class Mlp(nn.Module):
-    """
-    Implementation of MLP with 1*1 convolutions.
-    Input: tensor with shape [B, C, H, W]
-    """
+        y_pred = model(x)
+        loss = loss_fn(y_pred, y)
 
-    def __init__(self, in_features, hidden_features=None,
-                 out_features=None, act_layer=nn.GELU, drop=0., mid_conv=False):
-        super().__init__()
-        out_features = out_features or in_features
-        hidden_features = hidden_features or in_features
-        self.mid_conv = mid_conv
-        self.fc1 = nn.Conv2d(in_features, hidden_features, 1)
-        self.act = act_layer()
-        self.fc2 = nn.Conv2d(hidden_features, out_features, 1)
-        self.drop = nn.Dropout(drop)
-        self.apply(self._init_weights)
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
 
-        if self.mid_conv:
-            self.mid = nn.Conv2d(hidden_features, hidden_features, kernel_size=3, stride=1, padding=1,
-                                 groups=hidden_features)
-            self.mid_norm = nn.BatchNorm2d(hidden_features)
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * model.batch_size + len(x)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
-        self.norm1 = nn.BatchNorm2d(hidden_features)
-        self.norm2 = nn.BatchNorm2d(out_features)
+def test_loop(dataloader, model, loss_fn, device):
+    model.eval()
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    test_loss, correct = 0, 0
 
-    def _init_weights(self, m):
-        if isinstance(m, nn.Conv2d):
-            trunc_normal_(m.weight, std=.02)
-            if m.bias is not None:
-                nn.init.constant_(m.bias, 0)
+    # Evaluating the model with torch.no_grad() ensures that no gradients are computed during test mode
+    # also serves to reduce unnecessary gradient computations and memory usage for tensors with requires_grad=True
+    with torch.no_grad():
+        for x, y in dataloader:
+            x = x.to(device)
+            y = transform_y(y)
+            y = y.to(device)
+            pred = model(x)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
 
-    def forward(self, x):
-        x = self.fc1(x)
-        x = self.norm1(x)
-        x = self.act(x)
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
-        if self.mid_conv:
-            x_mid = self.mid(x)
-            x_mid = self.mid_norm(x_mid)
-            x = self.act(x_mid)
-        x = self.drop(x)
-
-        x = self.fc2(x)
-        x = self.norm2(x)
-
-        x = self.drop(x)
-        return x
+def transform_y(y):
+    mapping = {2: 0, 10: 1, 13: 2, 18: 3, 20: 4, 25: 5, 26: 6, 31: 7, 39: 8, 4: 9, 5: 10, 8: 11, 9: 12, 11: 13, 17: 14, 28: 15, 32: 16, 33: 17, 35: 18, 1: 19, 3: 20, 12: 21, 15: 22, 23: 23, 7: 24, 27: 25, 0: 26, 6: 27, 14: 28, 16: 29, 21: 30, 22: 31, 24: 32, 30: 33, 36: 34, 19: 35, 29: 36, 34: 37, 37: 38, 38: 39}
+    #attributes = ['5_o_Clock_Shadow', 'Arched_Eyebrows', 'Attractive', 'Bags_Under_Eyes', 'Bald', 'Bangs', 'Big_Lips', 'Big_Nose', 'Black_Hair', 'Blond_Hair', 'Blurry', 'Brown_Hair', 'Bushy_Eyebrows', 'Chubby', 'Double_Chin', 'Eyeglasses', 'Goatee', 'Gray_Hair', 'Heavy_Makeup', 'High_Cheekbones', 'Male', 'Mouth_Slightly_Open', 'Mustache', 'Narrow_Eyes', 'No_Beard', 'Oval_Face', 'Pale_Skin', 'Pointy_Nose', 'Receding_Hairline', 'Rosy_Cheeks', 'Sideburns', 'Smiling', 'Straight_Hair', 'Wavy_Hair', 'Wearing_Earrings', 'Wearing_Hat', 'Wearing_Lipstick', 'Wearing_Necklace', 'Wearing_Necktie', 'Young']
+    #new_attributes = ['Attractive', 'Blurry', 'Chubby', 'Heavy_Makeup', 'Male', 'Oval_Face', 'Pale_Skin', 'Smiling', 'Young', 'Bald', 'Bangs', 'Black_Hair', 'Blond_Hair', 'Brown_Hair', 'Gray_Hair', 'Receding_Hairline', 'Straight_Hair', 'Wavy_Hair', 'Wearing_Hat', 'Arched_Eyebrows', 'Bags_Under_Eyes', 'Bushy_Eyebrows', 'Eyeglasses', 'Narrow_Eyes', 'Big_Nose', 'Pointy_Nose', '5_o_Clock_Shadow', 'Big_Lips', 'Double_Chin', 'Goatee', 'Mouth_Slightly_Open', 'Mustache', 'No_Beard', 'Sideburns', 'Wearing_Lipstick', 'High_Cheekbones', 'Rosy_Cheeks', 'Wearing_Earrings', 'Wearing_Necklace', 'Wearing_Necktie']
+    #y = {attributes.index(v): new_attributes.index(v) for k, v in y.items()}
+    for batch, x in enumerate(y):
+        new_batch = torch.zeros(40, dtype=torch.float32)
+        for i in range(40):
+            new_batch[mapping[i]] = x[i]
+        y[batch] = new_batch
+    return y.to(torch.float32)
